8384ad3845b4119697bbe647cb76c3b1fba58723
Felipe Flamarini <felipeflamarini@hotmail.com>
2025-03-06 09:05:17 -0300

    refactoring: addressing folders and docker changes

--- delete_project.sh
+++ delete_project.sh
@@ -6,15 +6,17 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-LOGFILE="/var/log/delete_project.log"
-exec > >(tee -a "$LOGFILE") 2>&1
-
 validate_project_name() {
     [[ "$1" =~ ^[a-zA-Z0-9._-]+$ ]] || {
         echo "Invalid project name."
         exit 1
     }
 }
+
+ALL_PROJECTS_DIR="/etc/projects"
+
+LOGFILE="/var/log/delete_project.log"
+exec > >(tee -a "$LOGFILE") 2>&1
 
 read -p "Enter project name to delete: " PROJECT_NAME
 
@@ -24,17 +26,19 @@ if [ -z "$PROJECT_NAME" ]; then
 fi
 
 validate_project_name "$PROJECT_NAME"
+HOME = "$ALL_PROJECTS_DIR/$PROJECT_NAME"
 
-SERVICE_FILE="/etc/systemd/system/$PROJECT_NAME.service"
-PROJECT_DIR="/projects/$PROJECT_NAME"
-SUDOERS_FILE="/etc/sudoers.d/$PROJECT_NAME"
+SERVICE_FILE="$HOME/.config/systemd/user/$PROJECT_NAME.service"
+PROJECT_DIR="/etc/projects/$PROJECT_NAME"
 
 if systemctl is-enabled "$PROJECT_NAME.service" &>/dev/null; then
-    systemctl disable --now "$PROJECT_NAME.service"
+    su - "$PROJECT_NAME" -c "systemctl disable --now $PROJECT_NAME.service"
+    rm "$SERVICE_FILE"
 fi
 
-if [ -f "$SERVICE_FILE" ]; then
-    rm "$SERVICE_FILE"
+if systemctl --user is-active docker.service &>/dev/null; then
+    su - "$PROJECT_NAME" -c "systemctl --user disable --now docker.socket"
+    su - "$PROJECT_NAME" -c "systemctl --user disable --now docker.service"
 fi
 
 systemctl daemon-reload
@@ -45,10 +49,6 @@ fi
 
 if [ -d "$PROJECT_DIR" ]; then
     rm -rf "$PROJECT_DIR"
-fi
-
-if [ -f "$SUDOERS_FILE" ]; then
-    rm "$SUDOERS_FILE"
 fi
 
 echo "Project $PROJECT_NAME removed."
