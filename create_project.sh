7cd2a67f26c10cf50e7b558f3ffdfe5449156c1f
Felipe Flamarini <felipeflamarini@hotmail.com>
2025-03-06 08:52:46 -0300

    refactoring: service unit is executed by user instead of root

--- create_project.sh
+++ create_project.sh
@@ -6,9 +6,6 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-LOGFILE="/var/log/create_project.log"
-exec > >(tee -a "$LOGFILE") 2>&1
-
 validate_project_name() {
     [[ "$1" =~ ^[a-zA-Z0-9._-]+$ ]] || {
         echo "Invalid project name."
@@ -20,18 +17,24 @@ generate_password() {
     tr -dc 'A-Za-z0-9!@#$%^&*()_+' </dev/urandom | head -c 12
 }
 
+ALL_PROJECTS_DIR="/etc/projects"
+mkdir -p "$ALL_PROJECTS_DIR"
+
+LOGFILE="/var/log/create_project.log"
+exec > >(tee -a "$LOGFILE") 2>&1
+
 read -p "Enter project name: " PROJECT_NAME
 validate_project_name "$PROJECT_NAME"
 
-PROJECT_DIR="/projects/$PROJECT_NAME"
+HOME="$ALL_PROJECTS_DIR/$PROJECT_NAME"
 
 if id -u "$PROJECT_NAME" >/dev/null 2>&1; then
     echo "Error: User $PROJECT_NAME already exists."
     exit 1
 fi
 
-if [ -d "$PROJECT_DIR" ]; then
-    echo "Error: Directory $PROJECT_DIR already exists."
+if [ -d "$HOME" ]; then
+    echo "Error: Directory $HOME already exists."
     exit 1
 fi
 
@@ -40,11 +43,13 @@ read -p "Enter stop command: " STOP_COMMAND
 
 PASSWORD=$(generate_password)
 
-useradd -m -d "$PROJECT_DIR" -s /bin/bash "$PROJECT_NAME"
+useradd -m -d "$HOME" -s /bin/bash "$PROJECT_NAME"
 echo "$PROJECT_NAME:$PASSWORD" | chpasswd
-chown -R "$PROJECT_NAME":"$PROJECT_NAME" "$PROJECT_DIR"
+chown -R "$PROJECT_NAME":"$PROJECT_NAME" "$HOME"
 
-SERVICE_FILE="/etc/systemd/system/$PROJECT_NAME.service"
+SERVICE_DIR="$HOME/.config/systemd/user"
+mkdir -p "$SERVICE_DIR"
+SERVICE_FILE="$SERVICE_DIR/$PROJECT_NAME.service"
 cat <<EOF >"$SERVICE_FILE"
 [Unit]
 Description=$PROJECT_NAME service
@@ -53,18 +58,18 @@ Description=$PROJECT_NAME service
 ExecStart=$START_COMMAND
 ExecStop=$STOP_COMMAND
 User=$PROJECT_NAME
-WorkingDirectory=$PROJECT_DIR
+WorkingDirectory=$HOME
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
+loginctl enable-linger "$PROJECT_NAME"
 systemctl daemon-reload
-
-echo "$PROJECT_NAME ALL=(ALL) NOPASSWD: /bin/systemctl start $PROJECT_NAME.service, /bin/systemctl stop $PROJECT_NAME.service, /bin/systemctl disable $PROJECT_NAME.service, /bin/systemctl enable $PROJECT_NAME.service" >/etc/sudoers.d/"$PROJECT_NAME"
+su - "$PROJECT_NAME" -c "systemctl --user enable $PROJECT_NAME.service"
 
 echo "Project $PROJECT_NAME created."
-echo "Directory: $PROJECT_DIR"
+echo "Directory: $HOME"
 echo "User: $PROJECT_NAME"
 echo "Password: $PASSWORD"
 echo "Service: $PROJECT_NAME.service"
