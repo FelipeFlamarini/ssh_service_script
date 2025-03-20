a86088093bb94d6d6630f9d08df819b4be34c085
Felipe Flamarini <felipeflamarini@hotmail.com>
2025-03-20 11:50:05 -0300

    feat: enhance create_project.sh with color output and optional commands

--- create_project.sh
+++ create_project.sh
@@ -6,6 +6,11 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
+red='\e[91m' #Red
+grn='\e[92m' #Green
+blu='\e[94m' #Blue
+DEF='\e[0m'  #Default color and effects
+
 validate_project_name() {
     [[ "$1" =~ ^[a-zA-Z0-9._-]+$ ]] || {
         echo "Invalid project name."
@@ -52,29 +57,35 @@ while true; do
     esac
 done
 
-read -p "Enter start command: " START_COMMAND
-read -p "Enter stop command: " STOP_COMMAND
+read -p "Enter start command (optional, blank to skip): " START_COMMAND
+read -p "Enter stop command (optional, blank to skip): " STOP_COMMAND
 
 PASSWORD=$(generate_password)
 SERVICE_DIR="$HOME/.config/systemd/user"
 SERVICE_FILE="$SERVICE_DIR/$PROJECT_NAME.service"
 PROJECT_DIR="$HOME/project"
+BASHRC_FILE="$HOME/.bashrc"
 
 useradd -m -d "$HOME" -s /bin/bash "$PROJECT_NAME"
 echo "$PROJECT_NAME:$PASSWORD" | chpasswd
 mkdir -p "$SERVICE_DIR"
 mkdir -p "$PROJECT_DIR"
+chown -R "$PROJECT_NAME" "$SERVICE_DIR"
 
 if [ "$NEEDS_DOCKER" == "y" ]; then
-    if [ -d /usr/bin/dockerd-rootless-setuptool.sh ]; then
-        sudo machinectl shell $PROJECT_NAME@ /bin/bash -c "dockerd-rootless-setuptool.sh install"
-        sudo machinectl shell $PROJECT_NAME@ /bin/bash -c "systemctl --user enable docker.socket"
-        sudo machinectl shell $PROJECT_NAME@ /bin/bash -c "systemctl --user enable --now docker.service"
+    printf "${blu}Installing Docker for $PROJECT_NAME...${DEF}\n"
+    if [ -f /usr/bin/dockerd-rootless-setuptool.sh ]; then
+        sudo machinectl shell $PROJECT_NAME@ /bin/bash -c "dockerd-rootless-setuptool.sh install" >/dev/null
+        echo "export PATH=/usr/bin:\$PATH" >>"$BASHRC_FILE"
+        echo "export DOCKER_HOST=unix:///run/user/$(id -u $PROJECT_NAME)/docker.sock" >>"$BASHRC_FILE"
+        printf "${grn}Docker installed.${DEF}\n"
+
     else
         echo "Error: dockerd-rootless-setuptool.sh couldn't be found, check if docker-ce-rootless-extras is installed."
     fi
 fi
 
+printf "${blu}Creating service file for $PROJECT_NAME...${DEF}\n"
 cat <<EOF >>"$SERVICE_FILE"
 [Unit]
 Description=$PROJECT_NAME service
@@ -96,13 +107,14 @@ WorkingDirectory=$PROJECT_DIR
 WantedBy=default.target
 EOF
 
-sudo machinectl shell $PROJECT_NAME@ /bin/bash -c "systemctl --user enable --now $PROJECT_NAME.service"
-
+printf "${blu}Enabling $PROJECT_NAME service...${DEF}\n"
+systemctl daemon-reload
+sudo machinectl shell $PROJECT_NAME@ /bin/bash -c "systemctl --user enable --now $PROJECT_NAME.service" >>/dev/null
 loginctl enable-linger "$PROJECT_NAME"
 chown -R "$PROJECT_NAME" "$HOME"
 
-echo "Project $PROJECT_NAME created."
-echo "Directory: $HOME"
+printf "${grn}Project $PROJECT_NAME created.${DEF}\n"
+echo "Home directory: $HOME"
 echo "User: $PROJECT_NAME"
 echo "Password: $PASSWORD"
-echo "Service: $PROJECT_NAME.service"
+echo "Service file: $SERVICE_FILE"
